1dead93d17f95a1776e36b3d50746c224ee9deb4
masaniwa <masaniwasdp@gmail.com>
2019-02-13 01:51:23 +0900

    Aldonis difinojn de la provoj.

--- source/sdlraii/testmock/sdl.d
+++ source/sdlraii/testmock/sdl.d
@@ -12,6 +12,13 @@ struct SDL_Cursor {}
 struct SDL_Joystick {}
 struct SDL_GameController {}
 struct SDL_Haptic {}
+struct Mix_Chunk {}
+struct Mix_Music {}
+struct TTF_Font {}
+struct UDPpacket {}
+struct _TCPsocket {}
+struct _UDPsocket {}
+struct _SDLNet_SocketSet {}
 
 FunctionMock!(const(char)*) SDL_GetError;
 FunctionMock!(void, SDL_Window*) SDL_DestroyWindow;
@@ -24,3 +31,11 @@ FunctionMock!(void, SDL_Cursor*) SDL_FreeCursor;
 FunctionMock!(void, SDL_Joystick*) SDL_JoystickClose;
 FunctionMock!(void, SDL_GameController*) SDL_GameControllerClose;
 FunctionMock!(void, SDL_Haptic*) SDL_HapticClose;
+FunctionMock!(void, Mix_Chunk*) Mix_FreeChunk;
+FunctionMock!(void, Mix_Music*) Mix_FreeMusic;
+FunctionMock!(void, TTF_Font*) TTF_CloseFont;
+FunctionMock!(void, UDPpacket*) SDLNet_FreePacket;
+FunctionMock!(void, UDPpacket**) SDLNet_FreePacketV;
+FunctionMock!(void, _TCPsocket*) SDLNet_TCP_Close;
+FunctionMock!(void, _UDPsocket*) SDLNet_UDP_Close;
+FunctionMock!(void, _SDLNet_SocketSet*) SDLNet_FreeSocketSet;
